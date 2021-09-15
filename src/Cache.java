9796019798d038e568e5ab43bb9e7e61fdc4edae
Mario Torres <mariotorres791@u.boisestate.edu>
2021-09-15 12:02:00 -0600

    committing recent changes

--- src/Cache.java
+++ src/Cache.java
@@ -1,31 +1,116 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 
-public class Cache {
+/**
+ * Cache is a "Cache" implementation using linked list data structure.
+ *
+ * @author Mario Torres
+ *
+ * @param <T>
+ */
+public class Cache<T> {
+    private LinkedList<T> cacheLinkedList;
     private int size;
-    private int sizeLimit;
-    private int index;
-    LinkedList<String> list = new LinkedList<String>();
-    public Cache(int sizeInput) {
-        sizeLimit = sizeInput;
+
+    /**
+     * Constructor: creates a Cache Object
+     */
+    public Cache(int size) {
+        cacheLinkedList = new LinkedList<T>();
+        this.size = size;
     }
-    public int getObject(String input) {
-        index = list.indexOf(input);
-        return index;
+
+    /**
+     * Returns Cache Linked List
+     *
+     * @return Cache Linked List
+     */
+    public LinkedList<T> getCacheLinkedList() {
+        return cacheLinkedList;
     }
-    public void addObject(String input) {
-        list.addFirst(input);
-        size+=1;
-        if (size > sizeLimit) {
-            list.removeLast();
-            size-=1;
-        }
+
+    /**
+     * Sets Cache Linked List
+     *
+     * @param cacheLinkedList
+     */
+    public void setCacheLinkedList(LinkedList<T> cacheLinkedList) {
+        this.cacheLinkedList = cacheLinkedList;
     }
-    public void removeObject(int input) {
-        list.remove(input);
-        size-=1;
+
+    /**
+     * Adds the element at the top of the Cache
+     *
+     * @param element which is added to the top of the Cache
+     */
+    public void addToCache(T element) {
+        cacheLinkedList.addFirst(element);
     }
+
+    /**
+     * Removes the last element in the Cache
+     */
+    public void removeLastCache() {
+        cacheLinkedList.removeLast();
+    }
+
+    /**
+     * Removes the element in the index of the Cache
+     *
+     * @param index is the index of the element to be removed
+     */
+    public void removeFromCache(int index) {
+        cacheLinkedList.remove(index);
+    }
+
+    /**
+     * Removes the element from the Cache
+     *
+     * @param element to be removed from the Cache
+     */
+    public void removeFromCache(T element) {
+        cacheLinkedList.remove(element);
+    }
+
+    /**
+     * Empties the Cache
+     */
     public void clearCache() {
-        list.clear();
-        size=0;
+        cacheLinkedList.clear();
+    }
+
+    /**
+     * Returns true if the Cache is full
+     *
+     * @return true if Cache is full
+     */
+    public boolean cacheFull() {
+        return (cacheLinkedList.size() == size);
+    }
+
+    /**
+     * Returns true if the Cache is empty
+     *
+     * @return true if the Cache is empty
+     */
+    public boolean emptyCache() {
+        return (cacheLinkedList.size() == 0);
+    }
+
+    /**
+     * Returns the Cache size
+     *
+     * @return the Cache size
+     */
+    public int cacheSize() {
+        return cacheLinkedList.size();
+
+    }
+
+    /**
+     * String output of the Cache Linked List
+     */
+    public String toString() {
+        return Arrays.deepToString(cacheLinkedList.toArray());
     }
 }
